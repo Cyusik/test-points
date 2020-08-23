8ca7318ca8786e11c90aa05513e2208b34e79295
Cyusik <dimonbrl99@gmail.com>
2020-08-23 07:58:37 +0300

    no message

--- points/script/swapform.php
+++ points/script/swapform.php
@@ -4,20 +4,22 @@ $query = "SELECT * FROM formobmen WHERE `open`";
 $link->set_charset("utf8");
 $result = mysqli_query($link, $query);
 if (isset($_POST['nicknames5']) && isset($_POST['login5'])  && isset($_POST['priz5']) && isset($_POST['points_search']) && isset($_POST['points_required'])){
-	$a = $_POST['points_search'];
-	$b = $_POST['points_required'];
-	echo $a.' :переменная: '.$b;
+	//echo $a.' :переменная: '.$b;
 	while($row = $result->fetch_assoc()) {
 		$open = $row['open'];
 		if($open == '1') {
-			$nicknames5 = trim(mysqli_real_escape_string($link, $_POST['nicknames5']));
-			$login5 = trim(mysqli_real_escape_string($link, $_POST['login5']));
+			$points_search = $_POST['points_search'];
+			$points_required = $_POST['points_required'];
+			$nicknames5 = trim(stripcslashes(mysqli_real_escape_string($link, $_POST['nicknames5'])));
+			$login5 = trim(stripcslashes(mysqli_real_escape_string($link, $_POST['login5'])));
+			// ------обработка массива-------------
 			foreach($_POST['priz5'] as $k=>$m) {
 				if (!empty($m)) {
 					$mass[$k] = $m;
 				}
 			}
 			$priz5 = implode("\r",$mass);
+			//-------конец обработки---------------
 			$link->query("INSERT INTO zapisform (nickname,account,priz) VALUES ('$nicknames5','$login5','$priz5')");
 			if($result) {
 				echo "<table class='table_dark2'>
