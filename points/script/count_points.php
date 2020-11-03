be9b805d25c21a4d4a5fc811f92ff9b644425283
Cyusik <dimonbrl99@gmail.com>
2020-11-03 22:00:47 +0300

    izi ipmort

--- points/script/count_points.php
+++ points/script/count_points.php
@@ -1,5 +1,5 @@
 <?php
-$start = microtime(true);
+//$start = microtime(true);
 set_time_limit(380);
 ini_set('memory_limit', '128M');
 
@@ -34,21 +34,40 @@ if (!empty($_FILES['countfile'])) {
 					}
 					$i++;
 				}
-				foreach($importData_arr as $k => $array) {
-					if ($array[0] == '' || NULL || false) { //удаляем пустые строки
-						unset($importData_arr[$k]);
-					}
-					if (stripos($array[1], 'Boss') !== false) { // поиск содержания boss
-						$importData_arr[$k][1] = '20';
+				if (!empty($importData_arr)) {
+					$check_ignore =  "SELECT nickname FROM tablballs WHERE exclude = '1'";
+					$result = mysqli_query($link, $check_ignore) or die ('Error: '.mysqli_error($link));
+					$rows = mysqli_num_rows($result);
+					if ($rows > 0) {
+						for($i = 0; $i < $rows; ++$i) {
+							$row = mysqli_fetch_row($result);
+							$ignore_list[] = $row;
+						}
+						function compare_ignoreList($a, $b) {
+							return strcmp($a[0], $b[0]);
+						}
+						$white_array = array_udiff($importData_arr, $ignore_list, 'compare_ignoreList'); // удалляем из массива список тех кто есть в листе игнора
+					} else {
+						echo 'Игнор лист пуст';
+						$white_array = $importData_arr; // если игнор лист пуст
 					}
 				}
-				foreach($importData_arr as $k => $array) {
+
+				foreach($white_array as $k => $array) {
+					if ($array[0] == '' || NULL || false) { //удаляем пустые строки
+						unset($white_array[$k]);
+					}
+					if (stripos($array[1], 'Boss') !== false) { // поиск содержания boss
+						$white_array[$k][1] = '20';
+					}
+				}
+				foreach($white_array as $k => $array) {
 					if (stripos($array[1], 'Courier') !== false) { // поиск обычных
-						$importData_arr[$k][1] = '15';
+						$white_array[$k][1] = '15';
 					}
 				}
 				$unique_array = array();
-				foreach($importData_arr as $data) { // удалляем дубликаты, складываем значения
+				foreach($white_array as $data) { // удалляем дубликаты, складываем значения
 					$hash = $data[0];
 					if (isset($unique_array[$hash])) {
 						$data[1] += $unique_array[$hash][1];
@@ -62,19 +81,28 @@ if (!empty($_FILES['countfile'])) {
 				}
 				$nums = count($unique_array);
 				echo 'sucsess<br>'.$nums.' строк<br>';
-				echo 'Время выполнения скрипта: '.round(microtime(true) - $start, 4).' сек.';
-				exit();
+				//echo 'Время выполнения скрипта: '.round(microtime(true) - $start, 4).' сек.';
+				//exit();
+				$delet_files = $count_files;
+				if (file_exists($delet_files)) {
+					unlink($delet_files);
+					echo "<b style='color:red'>Загрузка завершена</b>";
+				}
 
 			} else {
 				echo 'Ошибка загрузки файла в папку uploads';
 			}
 		}
-
-	} else {
-		echo '<br>';
-		echo 'нет такого';
+		$link->close();
+		$result->free();
+		unset($unique_array);
+		unset($white_array);
 	}
-
+	else
+	{
+		echo '<br>';
+		echo 'нет такого типа файла';
+	}
 } else
 {
 	echo 'Fail...';
