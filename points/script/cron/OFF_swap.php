1a7fc77346fab1bb5087df5599509f3249be81a7
Cyusik <dimonbrl99@gmail.com>
2020-11-26 00:08:57 +0300

    ффвм

--- points/script/cron/OFF_swap.php
+++ points/script/cron/OFF_swap.php
@@ -3,14 +3,13 @@ include_once '../../script/connect.php';
 $file_login = "../../logfiles/exchange_log.log";
 $fw = fopen($file_login, "a+");
 $date = date('Y-m-d h:i:s');
-$newdate = date('Y-m-d h:i:s A', strtotime($date));
-$recording = 2;
-$id = 1;
-$query ="UPDATE formobmen SET `open`= '$recording' WHERE id='$id'";
+$new_date = date('Y-m-d h:i:s A', strtotime($date));
+
+$query ="UPDATE `formobmen` SET `open`= 2 WHERE `id`=1";
 $result = mysqli_query($link, $query) or die('Error: '.mysqli_error($link));
+
 if($result){
-	fwrite($fw, $newdate.' CRON close swap => true'."\r\n");
+	fwrite($fw, $new_date.' CRON close swap => true'."\r\n");
 }
 $link ->close();
 fclose($fw);
-?>
--- points/script/cron/ON_swap.php
+++ points/script/cron/ON_swap.php
@@ -3,14 +3,13 @@ include_once '../../script/connect.php';
 $file_login = "../../logfiles/exchange_log.log";
 $fw = fopen($file_login, "a+");
 $date = date('Y-m-d h:i:s');
-$newdate = date('Y-m-d h:i:s A', strtotime($date));
-$recording = 1;
-$id = 1;
-$query ="UPDATE formobmen SET `open`= '$recording' WHERE id='$id'";
+$new_date = date('Y-m-d h:i:s A', strtotime($date));
+
+$query ="UPDATE `formobmen` SET `open`= 1 WHERE `id`=1";
 $result = mysqli_query($link, $query) or die('Error: '.mysqli_error($link));
+
 if($result){
-	fwrite($fw, $newdate.' CRON open swap => true'."\r\n");
+	fwrite($fw, $new_date.' CRON open swap => true'."\r\n");
 }
 $link ->close();
 fclose($fw);
-?>
